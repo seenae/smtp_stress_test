f91121e506acb9dbd94a138608c817af1065c0c5
krishna <krishnaklooper@gmail.com>
2018-11-16 20:27:49 +0530

    change parameter 1

--- stress_test.py
+++ stress_test.py
@@ -22,7 +22,7 @@ from opentsdb import TSDBClient
 PRESERVE_SESSION = True
 tsdb = TSDBClient('internal-hugemetric-1216732828.us-east-1.elb.amazonaws.com', static_tags={'node': 'OutBoundTestNode'})
 MAX_MAILS = 10
-MAX_RATE = 2
+MAX_RATE = 20
 SMTP_LOGIN_TIME = []
 SMTP_SENDMAIL_TIME = []
 MAX_CONCURRENT_CONNECTIONS = 200
